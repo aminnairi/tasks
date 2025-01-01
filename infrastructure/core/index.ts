0641c5e9c0e7a5f80b425e6cca3db5725ba9e560
aminnairi <18418459+aminnairi@users.noreply.github.com>
2025-01-01 20:51:57 +0100

    moved the route to its dedicated file

--- infrastructure/core/index.ts
+++ infrastructure/core/index.ts
@@ -1,46 +1,10 @@
-import { createApplication, createHttpRoute } from "@renkei/core"
-import { z } from "zod"
-
-export const [listTaskRoute, implementListTaskRoute] = createHttpRoute({
-  input: z.null().parse,
-  output: value => {
-    return z.union([
-      z.object({
-        success: z.literal(true),
-        tasks: z.array(z.object({
-          identifier: z.string(),
-          description: z.string(),
-          doneAt: z.union([
-            z.date({ coerce: true }),
-            z.null()
-          ]),
-          dueAt: z.union([
-            z.date({ coerce: true }),
-            z.null()
-          ]),
-          createdAt: z.date({ coerce: true }),
-          updatedAt: z.date({ coerce: true }),
-          creatorIdentifier: z.string(),
-          assigneeIdentifier: z.union([
-            z.string(),
-            z.null(),
-          ]),
-        })),
-      }),
-      z.object({
-        success: z.literal(false),
-        error: z.literal("STREAM_ERROR")
-      }),
-      z.object({
-        success: z.literal(false),
-        error: z.literal("UNEXPECTED_ERROR"),
-      }),
-    ]).parse(value);
-  }
-});
+import { createApplication } from "@renkei/core"
+import { listTaskRoute } from "./routes/listTasks";
+import { loginRoute } from "./routes/login";
 
 export const { createServer, createClient } = createApplication({
   routes: {
-    listTasks: listTaskRoute
+    listTasks: listTaskRoute,
+    login: loginRoute
   }
 });
