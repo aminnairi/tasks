33a8e8ca012b1c7da549733ff1310b52e3ed5939
aminnairi <18418459+aminnairi@users.noreply.github.com>
2025-01-01 20:46:15 +0100

    listing tasks now requires to be authenticated

--- application/usecases/ListTasksUsecase.ts
+++ application/usecases/ListTasksUsecase.ts
@@ -1,15 +1,43 @@
+import { UnauthorizedError } from "@todo/domain/errors/UnauthorizedError";
 import { TaskAggregate } from "../../domain/aggregates/TaskAggregate";
 import { EventRepository } from "../repositories/EventRepository";
 import { TasksResponse } from "../responses/TasksResponse";
+import { AuthenticationService } from "../services/AuthenticationService";
 import { EventParserService } from "../services/EventParserService";
+import { UserEntity } from "@todo/domain/entities/UserEntity";
 
 export class ListTasksUsecase {
   public constructor(
     private readonly eventRepository: EventRepository,
     private readonly eventParserService: EventParserService,
+    private readonly authenticationService: AuthenticationService,
   ) { }
 
-  public async execute() {
+  public async execute(authenticationToken: string) {
+    const userIdentifier = await this.authenticationService.verifyAuthenticationToken(authenticationToken);
+
+    if (!userIdentifier) {
+      return new UnauthorizedError();
+    }
+
+    const unparsedUserEvents = await this.eventRepository.fetchFromStream(`user-${userIdentifier}`);
+
+    if (unparsedUserEvents instanceof Error) {
+      return unparsedUserEvents;
+    }
+
+    const userEvents = this.eventParserService.parseUserEvents(unparsedUserEvents);
+
+    if (userEvents instanceof Error) {
+      return userEvents;
+    }
+
+    const user = UserEntity.fromEvents(userEvents);
+
+    if (user instanceof Error) {
+      return user;
+    }
+
     const unparsedTaskEvents = await this.eventRepository.fetchFromStream("task-");
 
     if (unparsedTaskEvents instanceof Error) {
@@ -30,14 +58,12 @@ export class ListTasksUsecase {
 
     const tasks = taskAggregate.listTasks();
 
-    const response: TasksResponse = {
-      tasks: tasks.map(task => {
-        return {
-          ...task,
-          description: task.description.value
-        }
-      })
-    };
+    const response: TasksResponse = tasks.map(task => {
+      return {
+        ...task,
+        description: task.description.value
+      }
+    });
 
     return response;
   }
