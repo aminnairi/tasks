f4b31bef8efa99adebe4bebbd57c8023747a1da6
aminnairi <18418459+aminnairi@users.noreply.github.com>
2025-01-01 20:52:23 +0100

    added initialization of the administrator

--- infrastructure/server/index.ts
+++ infrastructure/server/index.ts
@@ -1,67 +1,63 @@
-import { createServer, implementListTaskRoute } from "@todo/core";
+import { createServer } from "@todo/core";
 import { createNodeHttpServerAdapter } from "@renkei/node"
-import { ListTasksUsecase } from "@todo/application/usecases/ListTasksUsecase";
-import { MemoryEventRepository } from "@todo/adapters/repositories/MemoryEventRepository";
-import { ZodEventParserService } from "@todo/adapters/services/ZodEventParserService";
+import { listTasksImplementation } from "./implementations/listTasksImplementation";
+import { loginImplementation } from "./implementations/loginImplementation";
+import { InitializeAdministratorUsecase } from "@todo/application/usecases/InitializeAdministratorUsecase";
+import { eventRepository } from "./repositories/eventRepository";
+import { eventParserService } from "./services/eventParserService";
+import { passwordService } from "./services/passwordService";
+import { settings } from "./settings";
 import { exhaustive } from "exhaustive";
 
-const eventRepository = new MemoryEventRepository();
-const eventParserService = new ZodEventParserService();
+const main = async () => {
+  const initializeAdministratorUsecase = new InitializeAdministratorUsecase(
+    eventRepository,
+    eventParserService,
+    passwordService
+  );
 
-const listTasksImplementation = implementListTaskRoute(async () => {
-  try {
-    const listTasksUsecase = new ListTasksUsecase(eventRepository, eventParserService);
-    const tasks = await listTasksUsecase.execute();
+  const result = await initializeAdministratorUsecase.execute(
+    settings.administrator.username,
+    settings.administrator.password
+  );
 
-    if (tasks instanceof Error) {
-      return exhaustive(tasks.name, {
-        ApplyError: () => {
-          return {
-            success: false,
-            error: "STREAM_ERROR"
-          }
-        },
-        FetchFromStreamError: () => {
-          return {
-            success: false,
-            error: "STREAM_ERROR"
-          }
-        },
-        ParseError: () => {
-          return {
-            success: false,
-            error: "STREAM_ERROR"
-          }
-        }
-      });
+  if (result instanceof Error) {
+    exhaustive(result.name, {
+      FailedToStreamFailedError: () => {
+        console.error("Failed to fetch from the stream.");
+      },
+      FetchFromStreamError: () => {
+        console.error("Stream error, possible data corruption.");
+      },
+      ParseError: () => {
+        console.error("Failed to parse events from the stream, possible data corruption.");
+      }
+    });
+    return;
+  }
+
+  console.log("Administrator initialized successfully.");
+
+  const server = createServer({
+    adapter: createNodeHttpServerAdapter({
+      clients: [
+        "http://localhost:5173"
+      ]
+    }),
+    implementations: {
+      listTasks: listTasksImplementation,
+      login: loginImplementation
     }
+  });
 
-    return {
-      success: true,
-      tasks
-    } as const
-  } catch (error) {
-    return {
-      success: false,
-      error: "UNEXPECTED_ERROR"
-    } as const
-  }
+  await server.start({
+    port: 8000,
+    host: "0.0.0.0"
+  });
+
+  console.log("Server started on http://localhost:8000");
+}
+
+main().catch(error => {
+  console.error(error);
 });
-
-const server = createServer({
-  adapter: createNodeHttpServerAdapter({
-    clients: [
-      "http://localhost:5173"
-    ]
-  }),
-  implementations: {
-    listTasks: listTasksImplementation
-  }
-});
-
-await server.start({
-  port: 8000,
-  host: "0.0.0.0"
-});
-
-console.log("Server started on http://localhost:8000");
