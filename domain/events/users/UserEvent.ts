4221eac692ddbfee5f7c32b251b9a023b2ba3ff2
aminnairi <18418459+aminnairi@users.noreply.github.com>
2025-01-01 20:49:39 +0100

    added user updated event

--- domain/events/users/UserEvent.ts
+++ domain/events/users/UserEvent.ts
@@ -1,3 +1,6 @@
 import { UserCreatedEvent } from "./UserCreatedEvent";
+import { UserUpdatedEvent } from "./UserUpdatedEvent";
 
-export type UserEvent = UserCreatedEvent
+export type UserEvent =
+  | UserCreatedEvent
+  | UserUpdatedEvent
