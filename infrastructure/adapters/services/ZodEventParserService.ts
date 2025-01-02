070d5d97bba18d013fa402bb0456f4d64262a803
aminnairi <18418459+aminnairi@users.noreply.github.com>
2025-01-02 20:56:07 +0100

    added project parsing logic

--- infrastructure/adapters/services/ZodEventParserService.ts
+++ infrastructure/adapters/services/ZodEventParserService.ts
@@ -3,6 +3,7 @@ import { EventParserService } from "@todo/application/services/EventParserServic
 import { TaskEvent } from "@todo/domain/events/tasks/TaskEvent";
 import { UserEvent } from "@todo/domain/events/users/UserEvent";
 import { ParseError } from "@todo/domain/errors/ParseError";
+import { ProjectEvent } from "@todo/domain/events/projects/ProjectEvent";
 
 export class ZodEventParserService implements EventParserService {
   public parseUserEvents(events: unknown[]): ParseError | UserEvent[] {
@@ -65,4 +66,29 @@ export class ZodEventParserService implements EventParserService {
 
     return new ParseError
   }
+
+  public parseProjectEvents(events: unknown[]): ParseError | ProjectEvent[] {
+    const schema = z.array(z.object({
+      identifier: z.string(),
+      date: z.date({ coerce: true }),
+      version: z.literal(1),
+      type: z.literal("PROJECT_CREATED"),
+      data: z.object({
+        identifier: z.string(),
+        name: z.string(),
+        managerIdentifier: z.string(),
+        createdAt: z.date({ coerce: true }),
+        updatedAt: z.date({ coerce: true })
+      }),
+    })) satisfies ZodSchema<Array<ProjectEvent>>;
+
+    const validation = schema.safeParse(events);
+
+    if (validation.success) {
+      return validation.data;
+    }
+
+    return new ParseError(String(validation.error));
+  }
+
 }
