a2149c8bab0fcc26d78bd077bc10c85d9a4fa65a
aminnairi <18418459+aminnairi@users.noreply.github.com>
2025-01-02 12:10:58 +0100

    updated according to the recent enforcing of the new apply method

--- domain/entities/TaskEntity.ts
+++ domain/entities/TaskEntity.ts
@@ -1,10 +1,10 @@
 import { Entity } from "./Entity";
 import { TaskDescription } from "../values/TaskDescription"
 import { TaskEvent } from "../events/tasks/TaskEvent";
-import { TaskNotFoundError } from "../errors/TaskNotFoundError";
-import { TaskDescriptionTooShortError } from "../errors/TaskDescriptionTooShortError";
+import { TaskCreatedEvent } from "../events/tasks/TaskCreatedEvent";
+import { randomUUID } from "crypto";
 
-export class TaskEntity implements Entity {
+export class TaskEntity implements Entity<TaskEvent> {
   private constructor(
     public readonly identifier: string,
     public readonly description: TaskDescription,
@@ -27,6 +27,8 @@ export class TaskEntity implements Entity {
     updatedAt: Date,
     creatorIdentifier: string,
     assigneeIdentifier: string | null,
+    projectIdentifier: string,
+    categoryIdentifier: string,
   ) {
     const taskDescription = TaskDescription.from(description);
 
@@ -42,37 +44,78 @@ export class TaskEntity implements Entity {
       createdAt,
       updatedAt,
       creatorIdentifier,
-      assigneeIdentifier
+      assigneeIdentifier,
+      projectIdentifier,
+      categoryIdentifier
     );
   }
 
-  public static fromEvents(events: TaskEvent[]) {
-    const initialTask = new TaskNotFoundError as TaskNotFoundError | TaskDescriptionTooShortError | TaskEntity
+  public apply(event: TaskEvent) {
+    switch (event.type) {
+      case "TASK_CREATED":
+        return TaskEntity.from(
+          event.data.identifier,
+          event.data.description,
+          event.data.doneAt,
+          event.data.dueAt,
+          event.data.createdAt,
+          event.data.updatedAt,
+          event.data.creatorIdentifier,
+          event.data.assigneeIdentifier,
+          event.data.projectIdentifier,
+          event.data.categoryIdentifier,
+        );
+    }
+  }
 
-    const task = events.reduce((previousTask, event) => {
-      if (!previousTask) {
-        return previousTask;
-      }
-
-      switch (event.type) {
-        case "TASK_CREATED":
-          return TaskEntity.from(
-            event.data.identifier,
-            event.data.description,
-            event.data.doneAt,
-            event.data.dueAt,
-            event.data.createdAt,
-            event.data.updatedAt,
-            event.data.createdBy,
-            event.data.assignedTo,
-          )
-      }
-    }, initialTask);
+  public static create(
+    identifier: string,
+    description: string,
+    doneAt: Date | null,
+    dueAt: Date | null,
+    createdAt: Date,
+    updatedAt: Date,
+    creatorIdentifier: string,
+    assigneeIdentifier: string | null,
+    projectIdentifier: string,
+    categoryIdentifier: string,
+  ) {
+    const task = TaskEntity.from(
+      identifier,
+      description,
+      doneAt,
+      dueAt,
+      createdAt,
+      updatedAt,
+      creatorIdentifier,
+      assigneeIdentifier,
+      projectIdentifier,
+      categoryIdentifier
+    );
 
     if (task instanceof Error) {
       return task;
     }
 
-    return task;
+    const taskCreatedEvent: TaskCreatedEvent = {
+      date: new Date(),
+      identifier: randomUUID(),
+      type: "TASK_CREATED",
+      version: 1,
+      data: {
+        assigneeIdentifier: task.assigneeIdentifier,
+        categoryIdentifier: task.categoryIdentifier,
+        createdAt: task.createdAt,
+        creatorIdentifier: task.creatorIdentifier,
+        description: task.description.value,
+        doneAt: task.doneAt,
+        dueAt: task.dueAt,
+        identifier: task.identifier,
+        projectIdentifier: task.projectIdentifier,
+        updatedAt: task.updatedAt
+      },
+    }
+
+    return taskCreatedEvent;
   }
 }
