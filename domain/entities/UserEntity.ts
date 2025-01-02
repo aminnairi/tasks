45716a2ede369340b66ad25a8ed2d18e048cd07d
aminnairi <18418459+aminnairi@users.noreply.github.com>
2025-01-02 12:11:12 +0100

    updated according to the recent change in the entity interface enforcing the apply method

--- domain/entities/UserEntity.ts
+++ domain/entities/UserEntity.ts
@@ -1,13 +1,20 @@
 import { Entity } from "./Entity";
 import { UserEvent } from "../events/users/UserEvent"
-import { UserNotFoundError } from "../errors/UserNotFoundError";
-import { UserUpdatedEventData } from "../events/users/UserUpdatedEvent";
+import { Username } from "../values/Username";
+import { Password } from "../values/Password";
+import { UsernameTooShortError } from "../errors/UsernameTooShortError";
+import { ApplyError } from "../errors/ApplyError";
+import { PasswordDoesNotIncludeLowercaseLetterError } from "../errors/PasswordDoesNotIncludeLowercaseLetterError";
+import { PasswordDoesNotIncludeNumberError } from "../errors/PasswordDoesNotIncludeNumberError";
+import { PasswordDoesNotIncludeSymbolError } from "../errors/PasswordDoesNotIncludeSymbolError";
+import { PasswordDoesNotIncludeUppercaseLetterError } from "../errors/PasswordDoesNotIncludeUppercaseLetterError";
+import { PasswordTooShortError } from "../errors/PasswordTooShortError";
 
-export class UserEntity implements Entity {
+export class UserEntity implements Entity<UserEvent> {
   private constructor(
     public readonly identifier: string,
-    public readonly username: string,
-    public readonly password: string,
+    public readonly username: Username,
+    public readonly password: Password,
     public readonly createdAt: Date,
     public readonly updatedAt: Date,
     public readonly administrator: boolean,
@@ -15,14 +22,23 @@ export class UserEntity implements Entity {
 
   public static from(
     identifier: string,
-    username: string,
-    password: string,
+    usernameValue: string,
+    passwordValue: string,
     createdAt: Date,
     updatedAt: Date,
     administrator: boolean,
   ) {
-    // TODO: username object value
-    // TODO: password object value
+    const username = Username.from(usernameValue);
+
+    if (username instanceof Error) {
+      return username;
+    }
+
+    const password = Password.from(passwordValue);
+
+    if (password instanceof Error) {
+      return password;
+    }
 
     return new UserEntity(
       identifier,
@@ -35,42 +51,67 @@ export class UserEntity implements Entity {
   }
 
   public static fromEvents(events: UserEvent[]) {
+    const initialUser = new ApplyError([]) as UserEntity | ApplyError<UsernameTooShortError | PasswordTooShortError | PasswordDoesNotIncludeNumberError | PasswordDoesNotIncludeLowercaseLetterError | PasswordDoesNotIncludeUppercaseLetterError | PasswordDoesNotIncludeSymbolError>;
+
     const user = events.reduce((previousUser, event) => {
       switch (event.type) {
         case "USER_CREATED":
-          return UserEntity.from(
+          const user = UserEntity.from(
             event.data.identifier,
             event.data.username,
             event.data.password,
             event.data.createdAt,
             event.data.updatedAt,
             event.data.administrator,
-          )
+          );
+
+          if (user instanceof Error) {
+            // TODO: apply previous errors as well
+            return new ApplyError([user]);
+          }
+
+          return user;
 
         case "USER_UPDATED":
-          if (previousUser === null) {
+          if (previousUser instanceof Error) {
             return previousUser;
           }
 
-          return previousUser.update(event.data);
-      }
-    }, null as null | UserEntity);
+          const updatedUser = previousUser.apply(event);
 
-    if (!user) {
-      return new UserNotFoundError;
-    }
+          if (updatedUser instanceof Error) {
+            // TODO: apply previous errors as well
+            return new ApplyError([updatedUser]);
+          }
+
+          return updatedUser;
+      }
+    }, initialUser);
 
     return user;
   }
 
-  public update(properties: UserUpdatedEventData) {
-    return UserEntity.from(
-      properties.identifier,
-      properties.username ?? this.username,
-      properties.password ?? this.password,
-      properties.createdAt ?? this.createdAt,
-      properties.updatedAt ?? this.updatedAt,
-      properties.administrator ?? this.administrator
-    )
+  public apply(event: UserEvent) {
+    switch (event.type) {
+      case "USER_CREATED":
+        return UserEntity.from(
+          event.data.identifier,
+          event.data.username,
+          event.data.password,
+          event.data.createdAt,
+          event.data.updatedAt,
+          event.data.administrator,
+        );
+
+      case "USER_UPDATED":
+        return UserEntity.from(
+          event.data.identifier,
+          event.data.username ?? this.username.value,
+          event.data.password ?? this.password.value,
+          event.data.createdAt ?? this.createdAt,
+          event.data.updatedAt ?? this.updatedAt,
+          event.data.administrator ?? this.administrator
+        )
+    }
   }
 }
