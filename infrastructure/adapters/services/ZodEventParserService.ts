54c8a0a4351e38089b382dec50442f1c5d5ec877
aminnairi <18418459+aminnairi@users.noreply.github.com>
2025-01-02 20:56:46 +0100

    updated the property name of the assignee

--- infrastructure/adapters/services/ZodEventParserService.ts
+++ infrastructure/adapters/services/ZodEventParserService.ts
@@ -51,8 +51,8 @@ export class ZodEventParserService implements EventParserService {
         ]),
         createdAt: z.date({ coerce: true }),
         updatedAt: z.date({ coerce: true }),
-        assignedTo: z.union([
         creatorIdentifier: z.string(),
+        assigneeIdentifier: z.union([
           z.string(),
           z.null()
         ])
