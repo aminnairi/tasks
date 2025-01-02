c6b0d7d632d1fadf6089102241dc3866721a3405
aminnairi <18418459+aminnairi@users.noreply.github.com>
2025-01-02 20:50:15 +0100

    added a method to parse project events

--- application/services/EventParserService.ts
+++ application/services/EventParserService.ts
@@ -1,3 +1,4 @@
+import { ProjectEvent } from "@todo/domain/events/projects/ProjectEvent";
 import { TaskEvent } from "../../domain/events/tasks/TaskEvent";
 import { UserEvent } from "../../domain/events/users/UserEvent";
 import { ParseError } from "@todo/domain/errors/ParseError"
@@ -5,4 +6,5 @@ import { ParseError } from "@todo/domain/errors/ParseError"
 export interface EventParserService {
   readonly parseUserEvents: (events: unknown[]) => ParseError | UserEvent[];
   readonly parseTaskEvents: (events: unknown[]) => ParseError | TaskEvent[];
+  readonly parseProjectEvents: (events: unknown[]) => ParseError | ProjectEvent[];
 }
