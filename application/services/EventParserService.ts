2dde1bdeaf62939dfca330af8d6d5cbe368b3b9b
aminnairi <18418459+aminnairi@users.noreply.github.com>
2025-01-02 20:50:22 +0100

    added a method to parse category events

--- application/services/EventParserService.ts
+++ application/services/EventParserService.ts
@@ -2,9 +2,11 @@ import { ProjectEvent } from "@todo/domain/events/projects/ProjectEvent";
 import { TaskEvent } from "../../domain/events/tasks/TaskEvent";
 import { UserEvent } from "../../domain/events/users/UserEvent";
 import { ParseError } from "@todo/domain/errors/ParseError"
+import { CategoryEvent } from "@todo/domain/events/categories/CategoryEvent";
 
 export interface EventParserService {
   readonly parseUserEvents: (events: unknown[]) => ParseError | UserEvent[];
   readonly parseTaskEvents: (events: unknown[]) => ParseError | TaskEvent[];
   readonly parseProjectEvents: (events: unknown[]) => ParseError | ProjectEvent[];
+  readonly parseCategoryEvents: (events: unknown[]) => ParseError | CategoryEvent[];
 }
