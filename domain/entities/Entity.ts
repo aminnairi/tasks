714809eafd7ede5c207edfd9a498b114b6034120
aminnairi <18418459+aminnairi@users.noreply.github.com>
2025-01-02 12:09:46 +0100

    now enforcing all entities to handle the events

--- domain/entities/Entity.ts
+++ domain/entities/Entity.ts
@@ -1,5 +1,6 @@
 import { EventShape } from "../events/EventShape";
 
-export interface Entity {
+export interface Entity<Event extends EventShape<unknown, unknown, unknown>> {
   readonly identifier: string;
+  readonly apply: (event: Event) => Entity<Event> | Error
 }
