db3b2c9f8bd44506e1dd444c1e34d0572952d104
aminnairi <18418459+aminnairi@users.noreply.github.com>
2025-01-02 20:57:21 +0100

    added the route to create a project

--- infrastructure/core/index.ts
+++ infrastructure/core/index.ts
@@ -1,10 +1,12 @@
 import { createApplication } from "@renkei/core"
 import { listTaskRoute } from "./routes/listTasks";
 import { loginRoute } from "./routes/login";
+import { createProjectRoute } from "./routes/createProject";
 
 export const { createServer, createClient } = createApplication({
   routes: {
     listTasks: listTaskRoute,
-    login: loginRoute
+    login: loginRoute,
+    createProject: createProjectRoute
   }
 });
