f5bd56801a94d8dfcdf160b71e5ae97fe7584e4b
aminnairi <18418459+aminnairi@users.noreply.github.com>
2025-01-08 19:46:08 +0100

    implemented the entity interface for the project member entity

--- domain/entities/ProjectMemberEntity.ts
+++ domain/entities/ProjectMemberEntity.ts
@@ -1,10 +1,26 @@
+import { ProjectMemberCreatedEvent } from "../events/projectMembers/ProjectMemberCreatedEvent";
+import { ProjectMemberEvent } from "../events/projectMembers/ProjectMemberEvent";
 import { Entity } from "./Entity";
 
-export class ProjectMemberEntity implements Entity {
+export class ProjectMemberEntity implements Entity<ProjectMemberEvent> {
   private constructor(
     public readonly identifier: string,
     public readonly projectIdentifier: string,
+    public readonly userIdentifier: string,
     public readonly createdAt: Date,
     public readonly updatedAt: Date
   ) { }
+
+  public apply(event: ProjectMemberCreatedEvent) {
+    switch (event.type) {
+      case "PROJECT_MEMBER_CREATED":
+        return new ProjectMemberEntity(
+          event.data.identifier,
+          event.data.projectIdentifier,
+          event.data.userIdentifier,
+          event.data.createdAt,
+          event.data.updatedAt,
+        );
+    }
+  }
 }
