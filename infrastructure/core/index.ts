e089e8959f81a25544cc186858414069db2bbefa
aminnairi <18418459+aminnairi@users.noreply.github.com>
2025-01-08 17:04:03 +0100

    added a route for fetching the details for one project

--- infrastructure/core/index.ts
+++ infrastructure/core/index.ts
@@ -3,12 +3,14 @@ import { listTaskRoute } from "./routes/listTasks";
 import { loginRoute } from "./routes/login";
 import { createProjectRoute } from "./routes/createProject";
 import { listProjectsRoute } from "./routes/listProject";
+import { getProjectDetailsRoute } from "./routes/getProjectDetails";
 
 export const { createServer, createClient } = createApplication({
   routes: {
     listTasks: listTaskRoute,
     login: loginRoute,
     createProject: createProjectRoute,
-    listProjects: listProjectsRoute
+    listProjects: listProjectsRoute,
+    getProjectDetails: getProjectDetailsRoute
   }
 });
