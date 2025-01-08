f6babb3af3f7e9eb419b286d700316e4f765c340
aminnairi <18418459+aminnairi@users.noreply.github.com>
2025-01-08 14:12:00 +0100

    now returning a UnauthorizedError instead of false when verifying the authentication token

--- application/services/AuthenticationService.ts
+++ application/services/AuthenticationService.ts
@@ -1,4 +1,6 @@
+import { UnauthorizedError } from "@todo/domain/errors/UnauthorizedError"
+
 export interface AuthenticationService {
   readonly createAuthenticationToken: (userIdentifier: string) => Promise<string>
-  readonly verifyAuthenticationToken: (authenticationToken: string) => Promise<string | false>
+  readonly verifyAuthenticationToken: (authenticationToken: string) => Promise<string | UnauthorizedError>
 }
